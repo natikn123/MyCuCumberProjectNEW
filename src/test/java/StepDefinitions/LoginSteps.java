b5880b3d72fb15c94f822507e98f477b31041c3d
Natik <niftaliyevn@gmail.com>
2022-04-26 00:13:38 -0400

    new commit and push in log ins

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -48,5 +48,5 @@ public class LoginSteps {
     }
 
 //teting new git and lets see this
-    // icreated new branch and worked on it and pushing to master braanch
+    // icreated new branch and wo rked on it and pushing to master braanch
 }
